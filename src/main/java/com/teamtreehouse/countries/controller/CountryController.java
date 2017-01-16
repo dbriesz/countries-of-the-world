b4fa2ec392d82b177048810598d75e0807dabc9f
David Riesz <dbriesz@users.noreply.github.com>
2017-01-16 15:48:58 -0800

    Created index view to display all countries along with corresponding names and flag images for each country

--- src/main/java/com/teamtreehouse/countries/controller/CountryController.java
+++ src/main/java/com/teamtreehouse/countries/controller/CountryController.java
@@ -1,13 +1,32 @@
 package com.teamtreehouse.countries.controller;
 
+import com.teamtreehouse.countries.data.CountryRepository;
+import com.teamtreehouse.countries.model.Country;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class CountryController {
+    @Autowired
+    private CountryRepository countryRepository;
+
     @RequestMapping("/")
-    public String listCountries() {
+    public String listCountries(ModelMap modelMap) {
+        List<Country> allCountries = countryRepository.getAllCountries();
+        modelMap.put("countries",allCountries);
         return "index";
+    }
+
+    @RequestMapping("/country/{name}")
+    public String countryDetails(@PathVariable String name, ModelMap modelMap) {
+        Country country = countryRepository.findByName(name);
+        modelMap.put("country",country);
+        return "country-details";
     }
 }
