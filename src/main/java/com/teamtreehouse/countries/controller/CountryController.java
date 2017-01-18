28a9d55348c55a8136fc5e134ce5ccfe24b276a1
David Riesz <dbriesz@users.noreply.github.com>
2017-01-18 13:01:46 -0800

    Changed findByName() method to use a stream for filtering data for finding a single country using the country name.  Added comments to Java code.

--- src/main/java/com/teamtreehouse/countries/controller/CountryController.java
+++ src/main/java/com/teamtreehouse/countries/controller/CountryController.java
@@ -17,27 +17,42 @@ public class CountryController {
     @Autowired
     private CountryRepository countryRepository;
 
+    // Mapping for the index page
     @RequestMapping("/")
     public String listCountries(@RequestParam(name = "sort", defaultValue = "", required = false) String sort, ModelMap modelMap) {
         List<Country> countries;
         switch (sort) {
+
+            // If the sort selected is "population", the mapping is set to sort by population size by calling the sortByPopulation method
             case "population":
                 countries = countryRepository.sortByPopulation();
                 break;
+
+            /* If the sort selected is "alphabetical", the mapping is set to sort
+            alphabetically by population name by calling the sortByName method */
             case "alphabetical":
                 countries = countryRepository.sortByName();
                 break;
+
+            // Default mapping in no particular order
             default:
                 countries = countryRepository.getAllCountries();
                 break;
         }
+
+        // Adds the list of countries to the ModelMap and returns the index page
         modelMap.put("countries",countries);
         return "index";
     }
 
+    // Mapping to detail page
     @RequestMapping("/country/{name}")
     public String countryDetails(@PathVariable String name, ModelMap modelMap) {
+
+        // Calls the findByName method in order to set the detail page mapping to the individual country
         Country country = countryRepository.findByName(name);
+
+        // Add the specific country to the ModelMap and returns the country details page
         modelMap.put("country",country);
         return "country-details";
     }
--- src/main/java/com/teamtreehouse/countries/data/CountryRepository.java
+++ src/main/java/com/teamtreehouse/countries/data/CountryRepository.java
@@ -11,32 +11,28 @@ import java.util.stream.Collectors;
 public class CountryRepository {
     private static final List<Country> ALL_COUNTRIES = Arrays.asList(
             new Country("Brazil", 205823665, "Brasilia", Arrays.asList("Portuguese")),
-            new Country("Cameroon", 24360803, "Yaounde", Arrays.asList("English", "French")),
             new Country("Hungary", 9874784, "Budapest", Arrays.asList("Hungarian")),
             new Country("Mexico", 123166749, "Mexico City", Arrays.asList("Spanish")),
+            new Country("Cameroon", 24360803, "Yaounde", Arrays.asList("English", "French")),
             new Country("Uruguay", 3351016, "Montevideo", Arrays.asList("Spanish"))
     );
 
+    // Uses a stream to filter data for finding a single country by country name
     public Country findByName(String name) {
-        for(Country country : ALL_COUNTRIES) {
-            if (country.getName().equals(name)) {
-                return country;
-            }
-        }
-        return null;
+        return ALL_COUNTRIES.stream().filter(country -> country.getName().equals(name)).findFirst().orElseThrow(IllegalArgumentException::new);
     }
 
     public List<Country> getAllCountries() {
         return ALL_COUNTRIES;
     }
 
+    // Uses a stream to sort the list of countries by country name
     public List<Country> sortByName() {
-        List<Country> sortedAlphabetical = ALL_COUNTRIES.stream().sorted((c1, c2) -> c1.getName().compareTo(c2.getName())).collect(Collectors.toList());
-        return sortedAlphabetical;
+        return ALL_COUNTRIES.stream().sorted((c1, c2) -> c1.getName().compareTo(c2.getName())).collect(Collectors.toList());
     }
 
+    // Uses a stream to sort the list of countries by country population
     public List<Country> sortByPopulation() {
-        List<Country> sortedPopulation = ALL_COUNTRIES.stream().sorted((c1, c2) -> c1.getPopulationAsInt() - c2.getPopulationAsInt()).collect(Collectors.toList());
-        return sortedPopulation;
+        return ALL_COUNTRIES.stream().sorted((c1, c2) -> c1.getPopulationAsInt() - c2.getPopulationAsInt()).collect(Collectors.toList());
     }
 }
